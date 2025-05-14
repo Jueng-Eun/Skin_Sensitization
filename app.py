407f523c6b9ac70a8b3f0a5dbab1f561efb6be2f
Jueng-Eun <tnqkr125@gmail.com>
2025-05-14 14:28:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import pandas as pd
 with open("ghs_new_rf_model.pkl", "rb") as f:
     ghs_model = pickle.load(f)
 
+st.title("피부감작성(GHS) 예측기")
+
 mw = st.number_input("MW (분자량)", min_value=0.0, value=200.0)
 mp = st.number_input("MP (녹는점)", min_value=-100.0, value=50.0)
 st_val = st.number_input("ST (표면장력)", min_value=0.0, value=20.0)
