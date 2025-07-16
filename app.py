8ad7a8ceadf1cc5aa4395d3dbd11f89dbfc5528e
Jueng-Eun <tnqkr125@gmail.com>
2025-07-16 09:00:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ with open("ghs_new_rf_model.pkl", "rb") as f:
 
 st.title("피부감작성(GHS) 예측기")
 
-mw = st.number_input("MW (분자량)", min_value=0.0, value=200.0)
-mp = st.number_input("MP (녹는점)", min_value=-100.0, value=50.0)
-st_val = st.number_input("ST (표면장력)", min_value=0.0, value=20.0)
+mw = st.number_input("MW (분자량) (g/mol)", min_value=0.0, value=200.0)
+mp = st.number_input("MP (녹는점) (°C)", min_value=-100.0, value=50.0)
+st_val = st.number_input("ST (표면장력) (dyn/cm)", min_value=0.0, value=20.0)
 
 if st.button("예측하기"):
     features = np.array([[mw, mp, st_val]])
